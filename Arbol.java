afceff4fbdd7e715e6947db6854bf11f1712aea8
Jeferson Soria <soriaarandajeferson@gmail.com>
2023-08-25 17:07:40 -0500

    definimos la clase fruta y agregamos las variables

--- Arbol.java
+++ Arbol.java
@@ -2,7 +2,15 @@
 package arbol;
 
 class Fruta{
-
+    String nombre;
+    String color;
+    boolean esPequena;
+    
+    public Fruta (String nombre, String color, boolean esPequena){
+        this.nombre = nombre;
+        this.color = color;
+        this.esPequena = esPequena;
+    }
     
     
     
