10dae6490f7e7bbebdf5c3fbe4933216b6c7a278
Jeferson Soria <soriaarandajeferson@gmail.com>
2023-08-25 18:14:23 -0500

    importamos las clases necesarias

--- Arbol.java
+++
@@ -1,9 +0,0 @@
-
-package arbol;
-
-class Fruta{
-
-    
-    
-    
-}
---
+++ ArbolEntropia.java
@@ -0,0 +1,7 @@
+
+package arbol;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
